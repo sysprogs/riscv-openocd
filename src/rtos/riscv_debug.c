98edad8d433eefa7b74acefd690a00e7fa3ba5dd
Sysprogs <sysprogs@sysprogs.com>
2017-10-19 12:01:06 -0700

    Fixed builds on Windows

--- src/rtos/riscv_debug.c
+++ src/rtos/riscv_debug.c
@@ -37,6 +37,40 @@ static int riscv_create_rtos(struct target *target)
 	return JIM_OK;
 }
 
+#ifdef _WIN32
+//https://stackoverflow.com/questions/40159892/using-asprintf-on-windows
+
+int vasprintf(char **strp, const char *fmt, va_list ap) {
+    // _vscprintf tells you how big the buffer needs to be
+    int len = _vscprintf(fmt, ap);
+    if (len == -1) {
+        return -1;
+    }
+    size_t size = (size_t)len + 1;
+    char *str = malloc(size);
+    if (!str) {
+        return -1;
+    }
+    
+    int r = vsnprintf(str, len + 1, fmt, ap);
+    if (r == -1) {
+        free(str);
+        return -1;
+    }
+    *strp = str;
+    return r;
+}
+
+int asprintf(char **strp, const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    int r = vasprintf(strp, fmt, ap);
+    va_end(ap);
+    return r;
+}
+
+#endif
+
 int riscv_update_threads(struct rtos *rtos)
 {
 	LOG_DEBUG("Updating the RISC-V Hart List");
